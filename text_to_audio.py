267ef01017b605d243bff9ca6bef9746ff1f60e8
sclement2 <sclement2@gmail.com>
2025-03-29 02:38:56 -0300

    Add text to speech code

--- text_to_audio.py
+++ text_to_audio.py
@@ -2,3 +2,20 @@ import asyncio
 from openai import AsyncOpenAI
 from openai.helpers import LocalAudioPlayer
 from dotenv import load_dotenv
+
+load_dotenv()
+
+openai = AsyncOpenAI()
+
+async def text_to_audio(text, tone_and_style_insructions):
+    async with openai.audio.speech.with_streaming_response.create(
+        model="gpt-4o-mini-tts",
+        voice="coral",
+        input=text,
+        instructions=tone_and_style_insructions,
+        response_format="pcm",
+    ) as response:
+        await LocalAudioPlayer().play(response)
+
+if __name__ == "__main__":
+    asyncio.run(text_to_audio("Hello world!", "Enthusiastic voice."))
